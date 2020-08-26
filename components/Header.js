4245bb671deae519e18249f1fde1388ad62bd567
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-08-26 08:20:48 -0600

    style for header

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {Text, StyleSheet, Platform} from 'react-native';
 
 const Header = () => {
     return ( 
         <>
-        
+            <Text style={styles.encabezado}>Criptomonedas</Text>
         </>
      );
 }
+
+const styles = StyleSheet.create({
+    encabezado: {
+        paddingTop: Platform.OS === 'ios' ? 50 : 10,
+    }
+})
  
 export default Header;
