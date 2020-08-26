ffb2aac581b944e59805d27d924f75b5960dbff5
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-08-26 08:51:00 -0600

    Image added

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
     View,
     Text,
     StatusBar,
+    Image,
 } from "react-native";
 import Header from "./components/Header";
 
@@ -13,12 +14,21 @@ const App = () => {
   return (
     <>
       <Header/>
+      <Image
+        style={styles.imagen}
+        source={require('./assets/img/cryptomonedas.png')}
+      />
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  
+  imagen:{
+    width: '100%',
+    height: 150,
+    marginHorizontal: '2.5%',
+
+  }
 });
 
 export default App;
