fb5f2918649b05cf57592440374ad9041a42e9d6
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-08-26 09:26:59 -0600

    Adding native picker for platform

--- components/Formulario.js
+++ components/Formulario.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import {Text, View, StyleSheet} from 'react-native';
+import {Picker} from '@react-native-community/picker';
+
 const Formulario = () => {
     return ( 
         <>
             <View>
                 <Text style={styles.label}>Moneda</Text>
+                <Picker>
+                    <Picker.Item label="- Seleccione -" value="" />
+                    <Picker.Item label="Dolar de Estados Unidos" value="USD" />
+                    <Picker.Item label="Pesos Mexicano" value="MXN" />
+                    <Picker.Item label="Euro" value="EUR" />
+                    <Picker.Item label="Libra Esterlina" value="GBP" />
+                </Picker>
                 <Text style={styles.label}>Criptomoneda</Text>
             </View>
         </>
