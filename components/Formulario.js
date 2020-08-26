dd3b3a06566b21d8c63ad35732b3ed4c2aa998d5
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-08-26 09:42:33 -0600

    request to api

--- components/Formulario.js
+++ components/Formulario.js
@@ -1,14 +1,27 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 import {Picker} from '@react-native-community/picker';
+import axios from 'axios';
 
 const Formulario = () => {
     const [ moneda, setMoneda] = useState('');
     const [ criptomoneda, setCriptomoneda] = useState('');
+    const [ criptomonedas, setCriptomonedas] = useState('');
 
     const obtenerMonenda = moneda => {
         setMoneda(moneda);
     }
+
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD';
+            const response = await axios.get(url);
+            if(response.status === 200){
+                setCriptomonedas(response.data.Data);
+            }
+        }
+        consultarAPI();
+    }, [])
 
     return ( 
         <>
